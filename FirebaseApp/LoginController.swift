ecd26649d550025fa4f6668564c634f7dad2f4e1
pari <pari@paris-MacBook-Pro.local>
2017-08-15 13:20:00 +0530

    Just made half of a login view and trying to put animation on it.

---
+++ FirebaseApp/LoginController.swift
@@ -0,0 +1,70 @@
+ //
+//  LoginController.swift
+//  FirebaseApp
+//
+//  Created by pari on 15/08/17.
+//  Copyright © 2017 pari. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    let inputsContainerView : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let loginRegisterButton: UIButton = {
+       let button = UIButton(type: UIButtonType.system)
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.white, for: .normal)
+        return button
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
+        
+        setupInputContainerView()
+        setupLoginRegisterButton()
+    }
+
+    func setupInputContainerView(){
+        //need x,y,width,heigth constraints
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func setupLoginRegisterButton(){
+        //need x,y,width,heigth constraints
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant:30).isActive = true
+    }
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+}
+ 
+ extension UIColor {
+   
+    convenience init(r:CGFloat ,g:CGFloat ,b:CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+ }
